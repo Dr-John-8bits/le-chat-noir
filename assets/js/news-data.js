0518b9cba9c8d81262b8d335653a4cbfca3a2f15
Dr-John-8bit <cornegidouille@gmail.com>
2026-04-11 00:12:04 +0200

    Add La Grande Nuit news update

--- assets/js/news-data.js
+++ assets/js/news-data.js
@@ -1,5 +1,14 @@
 window.LCN_NEWS_ITEMS = [
   {
+    sortKey: "2026-04-11-b",
+    dateLabel: "11 avril 2026",
+    title: "La Grande Nuit étire ses durées",
+    lead:
+      "La Grande Nuit évolue et resserre désormais son écoute autour de pièces longues, pour accentuer son pouvoir d’immersion.",
+    body:
+      "Moins de coupures, davantage de souffle, plus de temps laissé aux formes lentes et aux déploiements nocturnes : l’émission privilégie désormais uniquement des morceaux de plus de dix minutes. Une manière d’ouvrir plus grand encore l’espace de la nuit, en laissant les textures, les dérives et les paysages sonores prendre pleinement leur temps.",
+  },
+  {
     sortKey: "2026-04-11",
     dateLabel: "11 avril 2026",
     title: "Resting Bell rejoint les présences à l’antenne",
